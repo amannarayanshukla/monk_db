551062a6e612df70bb84ab351c16aab404845d0b
amannarayanshukla <amanshukla18.as@gmail.com>
2026-01-27 22:23:54 +0530

    simpleArean header and class files added

---
+++ include/monkdb/simpleArena.h
@@ -0,0 +1,13 @@
+//
+// Created by aman shukla on 27/01/26.
+//
+
+#ifndef MONKDB_SIMPLEARENA_H
+#define MONKDB_SIMPLEARENA_H
+
+
+class simpleArena {
+};
+
+
+#endif //MONKDB_SIMPLEARENA_H
---
+++ src/simpleArena.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aman shukla on 27/01/26.
+//
+
+#include "simpleArena.h"
